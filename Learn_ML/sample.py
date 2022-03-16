5b1b8d291062d69a44375ec7a485980a2fcbc558
Vipul-Jirge <72574669+Vipul-Jirge@users.noreply.github.com>
2022-03-17 00:12:36 +0530

    able to plot map on graph

--- Learn_ML/sample.py
+++ Learn_ML/sample.py
@@ -1,1 +1,8 @@
-print("hi")
+import re
+regex = r"^([a-z|A-Z|0-9|_|-]+)@([a-z|A-Z|0-9]+)\.([a-z|A-Z]+)$"
+s = 'fsDZ89-_@ddFF330.extensiontnr'
+matches = re.search(regex, s)
+if matches == None or len(''.join(matches.groups())) + 2 != len(s):
+    print('False')
+else :
+    print('True')
